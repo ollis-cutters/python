deac2f3163560d95a36e5ee631b43096e05e27ef
Oliver Schneider <oliver@assarbad.net>
2023-07-03 23:23:57 +0200

    Minor adjustments to boostrap Python scripts even more easily

--- {{ cookiecutter.project_slug }}/{{cookiecutter.project_slug}}.py
+++ {{ cookiecutter.project_slug }}/{{cookiecutter.project_slug}}.py
@@ -14,42 +14,75 @@ __compatible__ = (
     (3, 11),
 )
 __doc__ = """
-=========
- PROGRAM
-=========
+===============================
+ {{cookiecutter.project_name}}
+===============================
+
+{{cookiecutter.project_short_description}}
 """
-import argparse  # noqa: F401
-import os  # noqa: F401
+import argparse
+
+# import os
+# import re
 import sys
+
+# from contextlib import suppress
+# from copy import deepcopy
+from functools import partial  # also cache
+from pathlib import Path
+
+# from pprint import pformat, pprint
+# from typing import Optional
+
+eprint = partial(print, file=sys.stderr)
 
 # Checking for compatibility with Python version
 if not sys.version_info[:2] in __compatible__:
-    sys.exit(
-        "This script is only compatible with the following Python versions: %s."
-        % (", ".join(["%d.%d" % (z[0], z[1]) for z in __compatible__]))
-    )  # pragma: no cover
+    sys.exit(f"This script is only compatible with the following Python versions: {', '.join([f'{z[0]}.{z[1]}' for z in __compatible__])}")  # pragma: no cover
+
+CONFIG_DEFAULTS = """
+[foobar]
+baz = %(thisdir)s/bla.txt
+"""
 
 
-def parse_args():
-    """ """
+def parse_options() -> argparse.Namespace:
+    """\
+        Initializes the ArgumentParser and ConfigParser and performs the parsing
+    """
     from argparse import ArgumentParser
+    from configparser import ConfigParser
+    from textwrap import dedent
 
-    parser = ArgumentParser(description="PROGRAM")
-    parser.add_argument(
-        "--nologo", action="store_const", dest="nologo", const=True, help="Don't show info about this script."
-    )
-    return parser.parse_args()
+    cfgname = Path(__file__).absolute().with_suffix(".ini")
+    cfg = ConfigParser(defaults={"thisdir": cfgname.parent}, delimiters=("=",))
+    cfg.read_string(dedent(CONFIG_DEFAULTS), "<DEFAULTS>")
+    parser = ArgumentParser(description="{{cookiecutter.project_short_description}}", add_help=False)
+    # Only add the configuration file argument for starters
+    parser.add_argument("-c", "--config", "--ini", action="store", default=cfgname, metavar="CFG", type=Path, help=f"The config file; defaults to {cfgname}")
+    partial_args = parser.parse_known_args()[0]
+    cfg.read(partial_args.config)
+
+    # Remaining command line options
+    parser.add_argument("-h", "--help", action="help", help="Show this help message and exit")
+    parser.add_argument("--nologo", action="store_const", dest="nologo", const=True, help="Don't show info about this script.")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Turn up verbosity to see more details of what is going on.")
+    return cfg, parser.parse_args()
 
 
-def main(**kwargs):
-    """ """
+def main() -> int:
+    """\
+        Very simply the main entry point to this script
+    """
     return 0
 
 
 if __name__ == "__main__":
-    args = parse_args()
+    global args
+    global cfg
+    cfg, args = parse_options()
     try:
-        sys.exit(main(**vars(args)))
+        sys.exit(main())
     except SystemExit:
         pass
     except ImportError:
@@ -57,5 +90,5 @@ if __name__ == "__main__":
     except RuntimeError:
         raise  # re-raise
     except Exception:
-        print(__doc__)
+        eprint(__doc__)
         raise  # re-raise
